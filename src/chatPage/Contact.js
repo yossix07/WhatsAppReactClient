1a11a2af58ca6d5af6fa7fa7946dea98c871dccb
Yossi Maatook <yossix07@gmail.com>
2022-05-08 18:41:21 +0300

    upload ex1 files

---
+++ src/chatPage/Contact.js
@@ -0,0 +1,24 @@
+import { Col, Row, ListGroup } from "react-bootstrap"
+
+function Contact(props) {
+
+    return (
+        <ListGroup.Item action href={"#".concat(props.link)} className="list_group_items">
+            <Row>
+                <Col sm={2}>
+                    <img src={props.image} className="rounded-circle user_img_msg" alt="contact" />
+                </Col>
+                <Col sm={10}>
+                    <div className="d-flex justify-content-between">
+                        <h5 className="mb-1">{props.username}</h5>
+                        <small>{props.lastTime}</small>
+                    </div>
+                    <p className="mb-1">{props.lastMsg}</p>
+                </Col>
+            </Row>
+        </ListGroup.Item>
+    )
+
+}
+
+export default Contact;
---
+++ src/chatPage/chatWindow/Recorder.js
@@ -0,0 +1,22 @@
+import useRecorder from "./UseRecorder";
+import "./Record.css"
+
+function Recorder(props) {
+  let [audioURL, isRecording, isRecorded, startRecording, stopRecording, sendRecording] = useRecorder(props.myUser, props.username, props.refreshChat, props.btnId);
+  return (
+    <div className="App popover-member">
+      <audio className="popover-member" src={audioURL} controls />
+      <button onClick={startRecording} disabled={isRecording} className="btn btn-outline-light btn-sm bg-dark rec_btn popover-member">
+        <i className="bi bi-record-circle popover-member"></i> start
+      </button>
+      <button onClick={stopRecording} disabled={!isRecording} className="btn btn-outline-light btn-sm bg-dark rec_btn popover-member">
+        <i className="bi bi-stop-circle popover-member"></i> stop
+      </button>
+      <button onClick={sendRecording} disabled={!isRecorded} className="btn btn-outline-light btn-sm bg-dark rec_btn popover-member">
+        <i className="bi bi-envelope popover-member"></i> send
+      </button>
+    </div>
+  );
+}
+
+export default Recorder;
