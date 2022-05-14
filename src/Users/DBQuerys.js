4f034a302dc673b9a3b428ebd8233dec52043226
Yossi Maatook <yossix07@gmail.com>
2022-05-14 15:49:41 +0300

    Login and contacts list working

--- src/Users/DBQuerys.js
+++ src/Users/DBQuerys.js
@@ -1,29 +1,94 @@
-const port = 5008;
+const port = 5146;
 const urlPrefix = "http://localhost:" + port + "/api";
 
+export async function addNewUserAsync(username, password) {
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ "id": username, "password": password })
+  };
 
-async function getJsonFromUrl(url) {
-  const response =
-    await fetch(url, {
-      headers: { 'Content-Type': 'application/json' }
-    }).then(res => res.json().then(data => (
-      {
-        data: data,
-        status: res.status
-      })
-    ))
-
-  return response.data;
+  const response = await fetch(urlPrefix + "/Users", requestOptions);
+  if (response.ok) {
+    const token = await response.text();
+    console.log("token in addNewUserAsync: " + token)
+    return token;
+  }
+  return -1;
 }
 
-// get the user's contacts
-export async function getContactsAsync() {
-  const contactsJson = await getJsonFromUrl(urlPrefix + "/contacts");
-  console.log(contactsJson);
+export async function LogInAsync(username, password) {
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ "id": username, "password": password })
+  };
+
+  const response = await fetch(urlPrefix + "/LogIn", requestOptions);
+  if (response.ok) {
+    const token = await response.text();
+    console.log("token in addNewUserAsync: " + token)
+    return token;
+  }
+  return -1;
 }
 
+
 // get the user's contacts
-export async function addContactAsync(username, contactUsername ,contactNickname, contactServer) {
+export async function getContactsAsync(token) {
+  console.log("token in getContactsAsync is:")
+  console.log(token);
+
+  const requestOptions = {
+    method: 'Get',
+    headers: {
+      'Content-Type': 'application/json',
+      "Authorization": `Bearer ${token}`
+    }
+  };
+
+  const response = await fetch(urlPrefix + "/contacts", requestOptions);
+  const json = await response.json()
+
+  console.log("User contacts are: ");
+  console.log(json);
+
+  return json;
+}
+
+// get the user's messages with contactId
+export async function getContactsMessagesAsync(token, contactId) {
+  console.log("token in getContactsMessagesAsync is:")
+  console.log(token);
+
+  const requestOptions = {
+    method: 'Get',
+    headers: {
+      'Content-Type': 'application/json',
+      "Authorization": `Bearer ${token}`
+    }};
+
+  const response = await fetch(urlPrefix + "/contacts/" + contactId + "/messages" , requestOptions);
+  const json = await response.json()
+
+  console.log("Messages with" + contactId +  " are: ");
+  console.log(json);
+
+  return json;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+export async function addContactAsync(username, contactUsername, contactNickname, contactServer) {
   const requestOptions = {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
